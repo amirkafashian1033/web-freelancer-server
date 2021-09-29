f3c3c4347d4871c398272348ba7470334fb2d8a6
amirkafashian1033 <amirkafashian@hotmail.com>
2021-09-29 03:57:43 -0700

    fee

---
+++ MadPay.Presentation/Controllers/ValuesController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MadPay.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<string>> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public async Task<string> Post([FromBody] string value)
+        {
+            return null;
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public async Task<string> Put(int id, [FromBody] string value)
+        {
+            return null;
+
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<string>> Delete(int id)
+        {
+            return null;
+        }
+    }
+}
